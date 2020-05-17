154336890c6242d1880920ebbd4b2830dc04bfa1
Gerard Hynes <gkhynes@gmail.com>
2020-05-17 18:43:19 +0100

    Add route to update todo

--- server/index.js
+++ server/index.js
@@ -43,6 +43,19 @@ app.get("/todos/:id", async (req, res) => {
   }
 });
 // Update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo was updated");
+  } catch (err) {
+    console.error(err);
+  }
+});
 // Delete a todo
 
 app.listen(5000, () => {
