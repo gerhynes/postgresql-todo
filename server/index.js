8cd44de89bea398a943293b8c5572c1d66d163a2
Gerard Hynes <gkhynes@gmail.com>
2020-05-17 13:30:52 +0100

    Connect server to database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const pool = requrire("./db");
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+// Create a todo
+// Get all todos
+// Get a todo
+// Update a todo
+// Delete a todo
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
