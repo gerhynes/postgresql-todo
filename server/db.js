4e40b8c3083c861706ada9a4a43852f1dd5004b4
Gerard Hynes <gkhynes@gmail.com>
2020-05-17 13:52:56 +0100

    Add create todo route

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "fccproject",
+  password: "thomasmerton",
   host: "localhost",
   port: 5432,
-  database: perntodo,
+  database: "perntodo",
 });
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const pool = requrire("./db");
+const pool = require("./db");
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // req.body
 
 // Routes
 // Create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    consoe.log(err.message);
+  }
+});
 // Get all todos
 // Get a todo
 // Update a todo
