158e8685de3a997da13c3a681a57736f53a4b6c3
Gerard Hynes <gkhynes@gmail.com>
2020-05-17 19:29:58 +0100

    Add input component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React, { Fragment } from "react";
+import InputTodo from "./components/inputTodo";
 import "./App.css";
 
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ client/src/components/inputTodo.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useState } from "react";
+
+const InputTodo = () => {
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center my-5">PostgreSQL Todo List</h1>
+      <form className="d-flex" onSubmit={handleSubmit}>
+        <input
+          className="form-control"
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success mx-3">Add</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
