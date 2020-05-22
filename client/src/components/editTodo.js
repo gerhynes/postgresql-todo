bc9b9e646aaf0cb0444d6f9e4453ddf480f07c50
Gerard Hynes <gkhynes@gmail.com>
2020-05-22 18:47:29 +0100

    Adjust modal styles

--- client/src/components/editTodo.js
+++ client/src/components/editTodo.js
@@ -37,9 +37,9 @@ const EditTodo = ({ todo, getTodos }) => {
       {showModal ? (
         <>
           <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+            <div className="relative w-auto my-6 mx-auto max-w-4xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none p-4">
+                <div className="flex items-center justify-between p-5 border-b border-solid border-gray-300 rounded-t">
                   <h3 className="text-lg font-semibold">Edit Todo</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 text-3xl leading-none font-semibold outline-none focus:outline-none"
